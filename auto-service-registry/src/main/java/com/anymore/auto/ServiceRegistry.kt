0893d30c8d014b657eb9c0dba9add5da99662854
anymore <any_lymao@qq.com>
2026-08-01 15:53:09 +0800

    release: publish 0.0.12 components

--- auto-service-registry/src/main/java/com/anymore/auto/ServiceRegistry.kt
+++ auto-service-registry/src/main/java/com/anymore/auto/ServiceRegistry.kt
@@ -1,11 +1,13 @@
 package com.anymore.auto
 
+import java.util.function.Supplier
+
 /**
  * Created by anymore on 2022/3/31.
  */
 object ServiceRegistry {
     @JvmStatic
-    fun <S> get(clazz: Class<S>, alias: String): List<SingletonServiceSupplier<S>> {
+    fun <S> get(clazz: Class<S>, alias: String): List<Supplier<S>> {
         throw UnsupportedOperationException()
     }
 }
